04a1ec2efede132b78f8cf46b34b258372fe4013
AlejandroDanielMorales <Alejandrod.morales@hotmail.com>
2025-06-09 17:23:32 -0300

    dependencies1

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const routes = require("./routes/index")
 const app = express()
+const cors = require("cors");
+app.use(cors());
+
 app.use(express.json());
 app.use("/api",routes) 
+
 
 
 
