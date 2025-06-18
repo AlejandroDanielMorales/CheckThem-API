1ce7609c697ff8f80077e2b44905ce6ccd5dd737
AlejandroDanielMorales <Alejandrod.morales@hotmail.com>
2025-06-17 21:25:45 -0300

    update en controlador

--- controllers/checks.controller.js
+++ controllers/checks.controller.js
@@ -3,13 +3,32 @@ const Checks = require("../models/check.model");
 // Crear un nuevo cheque
 async function createCheck(req, res) {
   try {
-    const newCheck = new Checks(req.body);
+    const today = new Date();
+
+    const expirationDate = new Date(req.body.dateOfExpiration);
+    const oneMonthAgo = new Date(today);
+    oneMonthAgo.setMonth(today.getMonth() - 1);
+
+    let state = "pending";
+
+    if (expirationDate < oneMonthAgo) {
+      state = "payed";
+    } else if (expirationDate >= oneMonthAgo && expirationDate <= today) {
+      state = "onPayDate";
+    }
+
+    const newCheck = new Checks({
+      ...req.body,
+      state,
+    });
+
     const savedCheck = await newCheck.save();
     res.status(201).json(savedCheck);
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
 }
+
 
 // Obtener todos los cheques
 async function getAllChecks(req, res) {
@@ -58,20 +77,16 @@ async function deleteCheck(req, res) {
 }
 async function updateExpiredChecks() {
   try {
-    const today = new Date();
-    const twoMonthsAgo = new Date(today);
+    const today = new Date();  
     const oneMonthAgo = new Date(today);
 
-    twoMonthsAgo.setDate(1); 
-    twoMonthsAgo.setMonth(today.getMonth() - 2);
 
-    oneMonthAgo.setDate(1);
     oneMonthAgo.setMonth(today.getMonth() - 1);
 
     // 1. Marcar como "payed" los cheques vencidos hace 2 meses o más
     const resultPayed = await Checks.updateMany(
       {
-        dateOfExpiration: { $lte: twoMonthsAgo },
+        dateOfExpiration: { $lte: oneMonthAgo },
         state: { $ne: "payed" },
       },
       {
@@ -80,19 +95,27 @@ async function updateExpiredChecks() {
     );
 
     // 2. Marcar como "onPayDate" los cheques vencidos hace al menos 1 mes (pero no más de 2)
-    const resultOnPayDate = await Checks.updateMany(
+   const resultOnPayDate = await Checks.updateMany(
+  {
+    $and: [
+      { dateOfExpiration: { $gt: oneMonthAgo } },
+      { dateOfExpiration: { $lte: today } },
+      { state: "pending" }
+    ]
+  },
+  {
+    $set: { state: "onPayDate" },
+  }
+); 
+const resultPending = await Checks.updateMany(
       {
-        dateOfExpiration: {
-          $lte: oneMonthAgo,
-          $gt: twoMonthsAgo, // ⚠️ para evitar pisar los ya marcados como "payed"
-        },
-        state: { $ne: "onPayDate" },
+        dateOfExpiration: { $gt: today },
       },
       {
-        $set: { state: "onPayDate" },
+        $set: { state: "pending" },
       }
     );
-
+    console.log(`✅ ${resultPending.modifiedCount} cheques marcados como "pending".`);
     console.log(`✅ ${resultPayed.modifiedCount} cheques marcados como "payed".`);
     console.log(`✅ ${resultOnPayDate.modifiedCount} cheques marcados como "onPayDate".`);
   } catch (error) {
